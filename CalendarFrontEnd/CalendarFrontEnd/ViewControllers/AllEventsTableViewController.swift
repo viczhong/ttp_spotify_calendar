d8456771e52364a0476446860fe68c97f0596e6e
Victor Zhong <viczhong@gmail.com>
2018-06-21 14:53:56 -0400

    Got rid of some comments

--- CalendarFrontEnd/CalendarFrontEnd/ViewControllers/AllEventsTableViewController.swift
+++ CalendarFrontEnd/CalendarFrontEnd/ViewControllers/AllEventsTableViewController.swift
@@ -70,23 +70,6 @@ class AllEventsTableViewController: UITableViewController {
         }
     }
 
-
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-     }
-     */
-
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-
-
     // MARK: - Navigation
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
